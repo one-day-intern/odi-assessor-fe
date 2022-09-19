2a2419f99b0d8099df931d9a2acbadb9688856eb
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-09-19 17:27:22 +0700

    [CHORE] setup and configure jest for testing

---
+++ pages/index.test.ts
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react"
+import Home from "./index";
+import "@testing-library/jest-dom";
+
+describe('Home', () => {
+  const test = "a";
+  it('renders a heading', () => {
+    expect(test).toBe("a");
+  })
+},
+);
+
