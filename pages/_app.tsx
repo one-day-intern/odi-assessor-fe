ed97f6e1b9035138c853a992a1bac8c93a10c7c9
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-22 14:05:16 +0700

    [REFACTOR] add react-toastify container to root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@context/Authentication";
 import { MotionConfig } from "framer-motion";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MotionConfig reducedMotion="user">
+      <ToastContainer containerId="root-toast"/>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
