4710fbfdf87d8212860d8f0cab27487a8a4a232c
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-27 15:15:33 +0700

    [SETUP] port icons from assessee app. These icons are for interactive quiz, response test, and video conference
    Co-authored-by: Rashad Aziz <rashad.aziz@ui.ac.id>

---
+++ components/shared/svg/AppIcons/ResponseTestIcon/ResponseTestIcon.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ResponseTestIcon from "./ResponseTestIcon";
+import { render } from "@testing-library/react";
+
+describe("Response Test Icon component test suite", () => {
+    test("test render component", () => {
+        render(<ResponseTestIcon />)
+    })
+})
