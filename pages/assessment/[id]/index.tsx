5255d61f04af326d91d4418742fb3c4ba28a9e6e
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-11-01 19:06:23 +0700

    [REFACTOR] add index.tsx on page routes
    The index.tsx file shows where the page routes is displayed

---
+++ pages/assessment/[id]/index.tsx
@@ -0,0 +1,13 @@
+import { ListOfAssessees } from '@components/features/Dashboard/ListOfAssessees'
+import { PageTemplate } from '@components/shared/layouts/PageTemplate'
+import React from 'react'
+
+const ListOfAssesseePage = () => {
+  return (
+    <PageTemplate>
+        <ListOfAssessees/>
+    </PageTemplate>
+  )
+}
+
+export default ListOfAssesseePage
