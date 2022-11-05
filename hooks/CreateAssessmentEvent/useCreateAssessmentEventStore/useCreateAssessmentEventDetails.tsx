f5e8fae9ee5e9c3c721c1b3abcfb5b17b780c354
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-11-05 08:52:08 +0700

    [GREEN] implements useCreateAssessmnetEventDetails hook, which stores all the data related to assessment event creation and validation

--- hooks/CreateAssessmentEvent/useCreateAssessmentEventStore/useCreateAssessmentEventDetails.tsx
+++ hooks/CreateAssessmentEvent/useCreateAssessmentEventStore/useCreateAssessmentEventDetails.tsx
@@ -1,44 +1,118 @@
 import { useState } from "react";
-
-interface ParticipantsManyToMany {
-    assessee_email: string;
-    assessor_email: string;
-
-}
-
-interface CreateAssessmentDetailsSubmission {
-    name: string;
-    start_date: string;
-    test_flow_id: string;
-    list_of_participants: ParticipantsManyToMany[];
-}
+import { v4 as uuidv4 } from "uuid";
 
 const initialState = {
-    name: "",
-    start_date: "",
-    test_flow_id: "",
-    list_of_participants: []
-}
+  name: "",
+  start_date: "",
+  test_flow: null,
+  list_of_participants: [] as ParticipantsManyToMany[],
+};
+
+const initialError = {
+  name: "",
+  start_date: "",
+  test_flow: "",
+  list_of_participants: "",
+};
 
 const useCreateAssessmentEventDetails = () => {
-    const [assessmentEventDetails, setAssessmentEventDetails] = useState<CreateAssessmentDetailsSubmission>(initialState);
-    const [assessmentEventErrors, setAssessmentEventErrors] = useState<CreateAssessmentDetailsSubmission>(initialState);
+  const [assessmentEventDetails, setAssessmentEventDetails] =
+    useState<CreateAssessmentDetailsSubmission>(initialState);
+  const [assessmentEventErrors, setAssessmentEventErrors] =
+    useState<CreateAssessmentDetailsErrors>(initialError);
 
-    const setAssessmentData = (key: keyof CreateAssessmentDetailsSubmission, value: string | ParticipantsManyToMany) => {
-        setAssessmentEventDetails(prev => ({
-            ...prev,
-            [key]: (key === "list_of_participants") ? [...prev.list_of_participants, value] : value
-        }))
-    };
+  const setAssessmentData = (
+    key: Omit<keyof CreateAssessmentDetailsSubmission, "list_of_participants">,
+    value: string | ParticipantsManyToMany | TestFlow
+  ) => {
+    setAssessmentEventDetails((prev) => ({
+      ...prev,
+      [key as keyof CreateAssessmentDetailsSubmission]: value,
+    }));
+  };
 
-    const setAssessmentErrors = (key: keyof CreateAssessmentDetailsSubmission, value: string) => {
-        setAssessmentEventErrors(prev => ({
-            ...prev,
-            [key]: value
-        }))
+  const validateParticipationBeforeSubmit = (
+    assesseeEmail: string,
+    assessorEmail: string
+  ): [
+    boolean,
+    {
+      assesseeEmailError: string;
+      assessorEmailError: string;
     }
+  ] => {
+    const errors = {
+      assesseeEmailError: "",
+      assessorEmailError: "",
+    };
+    const assesseeWithSameEmailExists = (
+      assessmentEventDetails.list_of_participants as ParticipantsManyToMany[]
+    ).some((participant) => participant.assessee_email === assesseeEmail);
+    const assessorWithSameEmailExists = (
+      assessmentEventDetails.list_of_participants as ParticipantsManyToMany[]
+    ).some((participant) => participant.assessor_email === assessorEmail);
 
-    return { assessmentData: assessmentEventDetails, setAssessmentData, assessmentErrors: assessmentEventErrors, setAssessmentErrors }
-}
+    if (assesseeWithSameEmailExists)
+      errors.assesseeEmailError =
+        "The assessee has already been assigned to this assessment event.";
+    if (assessorWithSameEmailExists)
+      errors.assessorEmailError =
+        "The assessor has already been assigned to this assessment event.";
 
-export { useCreateAssessmentEventDetails }
+    return [
+      !assesseeWithSameEmailExists && !assessorWithSameEmailExists,
+      errors,
+    ];
+  };
+
+  const addEmptyParticipation = () => {
+    setAssessmentEventDetails((prev) => ({
+      ...prev,
+      list_of_participants: [
+        { id: uuidv4() } as ParticipantsManyToMany,
+        ...prev.list_of_participants,
+      ],
+    }));
+  };
+
+  const updateParticipation = (participation: ParticipantsManyToMany) => {
+    setAssessmentEventDetails((prev) => ({
+      ...prev,
+      list_of_participants: prev.list_of_participants.map((participant) =>
+        participation.id === participant.id ? participation : participant
+      ),
+    }));
+  };
+
+  const removeParticipation = (participant: ParticipantsManyToMany) => {
+    setAssessmentEventDetails((prev) => ({
+      ...prev,
+      list_of_participants: prev.list_of_participants.filter(
+        (participants) => participants.id !== participant.id
+      ),
+    }));
+  };
+
+  const setAssessmentErrors = (
+    key: keyof CreateAssessmentDetailsSubmission,
+    value: string
+  ) => {
+    setAssessmentEventErrors((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  return {
+    assessmentData: assessmentEventDetails,
+    setAssessmentData,
+    assessmentErrors: assessmentEventErrors,
+    setAssessmentErrors,
+    removeParticipation,
+    addEmptyParticipation,
+    updateParticipation,
+    validateParticipationBeforeSubmit,
+  };
+};
+
+export { useCreateAssessmentEventDetails };
