37c7a87ddb67bf5b16bfb31a8399a884a338a365
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-11-05 08:55:57 +0700

    [GREEN] implement interactivity in AddParticipants
    Now, the process of adding participant is more interactive, as assessor/assessee can be created by clicking add, editing the assessee and assessor directly, and remove it by clicking the x button

--- components/features/AssessmentEvent/Create/AddParticipants/AddParticipants.tsx
+++ components/features/AssessmentEvent/Create/AddParticipants/AddParticipants.tsx
@@ -1,121 +1,97 @@
 import { Button } from "@components/shared/elements/Button";
-import { InputField } from "@components/shared/forms/InputField";
-import { useCreateAssessmentEventDetails } from "@hooks/CreateAssessmentEvent/useCreateAssessmentEventStore/useCreateAssessmentEventDetails";
-import usePostRequest from "@hooks/shared/usePostRequest";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 import styles from "./AddParticipants.module.css";
+import { ParticipationCard } from "../ParticipationCard";
 
-interface AddParticipantsProps {
+interface CreateAssessmentDetailsProps {
+  addEmptyParticipation: () => void;
+  removeParticipation: (participant: ParticipantsManyToMany) => void;
+  updateParticipation: (participation: ParticipantsManyToMany) => void;
   assessmentData: CreateAssessmentDetailsSubmission;
-  setAssessmentData: (
-    key: keyof CreateAssessmentDetailsSubmission,
-    value: string | ParticipantsManyToMany
-  ) => void;
-  assessmentErrors: CreateAssessmentDetailsSubmission;
-  setAssessmentErrors: (
-    key: keyof CreateAssessmentDetailsSubmission,
-    value: string
-  ) => void;
+  validateParticipationBeforeSubmit: (
+    assesseeEmail: string,
+    assessorEmail: string
+  ) => [
+    boolean,
+    {
+      assessorEmailError: string;
+      assesseeEmailError: string;
+    }
+  ];
+  selectStep: (id: number) => void;
 }
 
 const AddParticipants = ({
+  addEmptyParticipation,
+  removeParticipation,
+  updateParticipation,
   assessmentData,
-  setAssessmentData,
-  assessmentErrors,
-  setAssessmentErrors,
-}: AddParticipantsProps) => {
-  const CREATE_ASSESSMENT_EVENT_URL = "/assessment/assessment-event/create/";
-  const ADD_PARTICIPANTS_URL = "/assessment/assessment-event/add-participant/";
-
-  const [assesseeEmail, setAssesseeEmail] = useState("");
-  const [assessorEmail, setAssessorEmail] = useState("");
-
-  const router = useRouter();
-
-  const {
-    data: createAssessmentData,
-    error: createAssessmentError,
-    postData: createAssessmentPost,
-  } = usePostRequest<CreateAssessmentDetailsSubmission, AssessmentEventChoice>(
-    CREATE_ASSESSMENT_EVENT_URL,
-    {
-      requiresToken: true,
-    }
-  );
-
-  const {
-    data: addParticipantData,
-    error: addParticipantError,
-    postData: addParticipantPost,
-  } = usePostRequest<AddParticipantsRequest, unknown>(ADD_PARTICIPANTS_URL, {
-    requiresToken: true,
-  });
-
-  useEffect(() => {
-    if (createAssessmentData == null) return;
-
-    addParticipantPost!({
-      list_of_participants: assessmentData.list_of_participants,
-      assessment_event_id: createAssessmentData.event_id,
-    });
-    // eslint-disable-next-line
-  }, [createAssessmentData]);
-
-  useEffect(() => {
-    console.log(createAssessmentError);
-  }, [createAssessmentError]);
-
-  useEffect(() => {
-    if (addParticipantData == null) return;
-    router.push("/");
-
-  }, [addParticipantData, router]);
-
-  useEffect(() => {
-    console.log(addParticipantError)
-  }, [addParticipantError])
+  validateParticipationBeforeSubmit,
+  selectStep
+}: CreateAssessmentDetailsProps) => {
+  const [isEditing, setEditing] = useState(false);
 
   return (
     <div className={styles["name-form"]} data-testid="add-part">
       <h2 className={styles["form__heading"]}>Add Participants</h2>
       <div>
-        <form
-          method="POST"
-          onSubmit={(e) => {
-            e.preventDefault();
-            createAssessmentPost!(assessmentData);
-          }}
-        >
-          <InputField
-            label="Assessee Email"
-            onChange={(e) => setAssesseeEmail(e.target.value)}
-            value={assesseeEmail}
-          />
-          <InputField
-            label="Assessor Email"
-            onChange={(e) => setAssessorEmail(e.target.value)}
-            value={assessorEmail}
-          />
-          <Button type="button" onClick={
-            (e: Event) => {
-              e.preventDefault();
-              setAssesseeEmail("");
-              setAssessorEmail("");
-              setAssessmentData("list_of_participants", 
-              {
-                assessee_email: assesseeEmail,
-                assessor_email: assessorEmail,
-              },
-            );
-            }
-          } variant="secondary">
+        <div className={styles["form__div"]}>
+          <Button
+            type="button"
+            onClick={(e: Event) => {
+              if (isEditing) return;
+
+              addEmptyParticipation();
+              setEditing(true);
+            }}
+            variant="secondary"
+            style={{
+              width: "fit-content",
+              padding: "0.5rem 3rem",
+            }}
+          >
             <h2>Add</h2>
           </Button>
-          <Button type="submit" variant="primary">
-            <h2>Submit</h2>
+          <Button
+            type="button"
+            onClick={() => {
+              if (isEditing) {
+                toast.error(
+                  "Participants should be saved before reviewing the assessment event",
+                  {
+                    containerId: "root-toast",
+                    position: toast.POSITION.TOP_CENTER,
+                  }
+                );
+                return;
+              }
+
+              selectStep(2);
+            }}
+            variant="primary"
+            style={{
+              width: "fit-content",
+              padding: "0.5rem 3rem",
+            }}
+          >
+            <h2>Next</h2>
           </Button>
-        </form>
+        </div>
+        <div className={styles["separator"]}></div>
+
+        <div className={styles["participation"]}>
+          {assessmentData.list_of_participants.map((participation) => (
+            <ParticipationCard
+              key={participation.id}
+              {...participation}
+              updateParticipation={updateParticipation}
+              removeParticipation={removeParticipation}
+              validateParticipation={validateParticipationBeforeSubmit}
+              onSave={() => setEditing(false)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
