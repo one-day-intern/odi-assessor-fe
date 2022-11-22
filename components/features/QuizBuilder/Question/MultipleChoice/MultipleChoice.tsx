d8034ad70101e397ecf3444bec351aacb5702c12
rashadaziz <rashad.aziz@ui.ac.id>
2022-11-22 20:30:54 +0700

    [GREEN] implement MultipleChoice component
    This component will be the container of the ChoiceOption component and will hold the 'Add Option' button to add another ChoiceOption to the multiple choice question. Again this component does not manage its own state, it communicates with useQuizBuilder hook

--- components/features/QuizBuilder/Question/MultipleChoice/MultipleChoice.tsx
+++ components/features/QuizBuilder/Question/MultipleChoice/MultipleChoice.tsx
@@ -1,5 +1,17 @@
-
+import { Button } from "@components/shared/elements/Button";
 import React from "react";
+import ChoiceOption from "./ChoiceOption";
+import styles from "./MultipleChoice.module.css";
+
+const buttonStyles: React.CSSProperties = {
+  color: "var(--primary)",
+  background: "transparent",
+  fontWeight: "bold",
+  border: "2px solid currentColor",
+  fontSize: "1.4rem",
+  margin: 0,
+};
+
 interface Props {
   isPreview?: boolean;
   options: MultipleChoiceQuestionOption[];
@@ -15,7 +27,26 @@ const MultipleChoice: React.FC<Props> = ({
   onEditOption,
   onRemoveOption,
 }) => {
-  return <></>
+  const noOptionsDuringPreview = isPreview && options.length === 0;
+  return (
+    <div className={styles["multiple-choice_container"]}>
+      {options.map((option) => (
+        <ChoiceOption
+          key={option.id}
+          isPreview={isPreview}
+          option={option}
+          onEditOption={onEditOption}
+          onRemoveOption={onRemoveOption}
+        />
+      ))}
+      {!isPreview && (
+        <Button onClick={onAddOption} style={buttonStyles}>
+          + Add Option
+        </Button>
+      )}
+      {noOptionsDuringPreview && "-"}
+    </div>
+  );
 };
 
 export default MultipleChoice;
