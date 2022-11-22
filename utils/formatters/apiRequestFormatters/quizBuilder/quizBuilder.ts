4587f07910fcb805cf9eb2ea66dbac82c7571a15
rashadaziz <rashad.aziz@ui.ac.id>
2022-11-22 17:49:49 +0700

    [GREEN] implement quiz builder api request formatter
    this formatter will make sure that the data strcture will match the one expected by the server when the quiz is being posted

--- utils/formatters/apiRequestFormatters/quizBuilder/quizBuilder.ts
+++ utils/formatters/apiRequestFormatters/quizBuilder/quizBuilder.ts
@@ -12,6 +12,32 @@ const quizBuilderPostFormatter = (
     total_points: 0,
   };
 
+  for (let question of questions) {
+    const isMultipleChoice = Object.hasOwn(question, "options");
+    const isEssay = Object.hasOwn(question, "answerKey");
+    body.total_points += question.points!;
+    if (isMultipleChoice) {
+      const mcq: MultipleChoiceQuestionApiRequest = {
+        points: question.points!,
+        prompt: question.prompt!,
+        question_type: "multiple_choice",
+        answer_options: [],
+      };
+      for (let option of (question as MultipleChoiceQuestion).options) {
+        mcq.answer_options.push({ content: option.content, correct: option.correct });
+      }
+      body.questions.push(mcq);
+    } else if (isEssay) {
+      const essay: EssayQuestionApiRequest = {
+        points: question.points!,
+        prompt: question.prompt!,
+        question_type: "text",
+        answer_key: (question as EssayQuestion).answerKey,
+      }
+      body.questions.push(essay);
+    }
+  }
+
   return body;
 };
 
