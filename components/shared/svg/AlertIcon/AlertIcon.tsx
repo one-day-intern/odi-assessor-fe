29e60913de42f45ac756dad3ab2d9cbc9189b0b3
rashadaziz <rashad.aziz@ui.ac.id>
2022-11-22 16:34:29 +0700

    [SETUP] add icons and svg for quiz builder

---
+++ components/shared/svg/AlertIcon/AlertIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AlertIcon: React.FC<ApplicationIconProps> = ({ width, height, color }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill={color ?? "black"} viewBox="0 0 24 24">
+      <g data-name="Layer 2">
+        <g data-name="alert-circle">
+          <rect width={width ?? "24"} height={height ?? "24"} opacity="0" />
+          <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z" />
+          <circle cx="12" cy="16" r="1" />
+          <path d="M12 7a1 1 0 0 0-1 1v5a1 1 0 0 0 2 0V8a1 1 0 0 0-1-1z" />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default AlertIcon;
