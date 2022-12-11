227c94e01c2a6b9a85c2c6f8eb20d5814cf791e2
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-12-11 12:13:01 +0700

    [REFACTOR] refactor hook for context

--- context/Signup/AssessorSignupStepContext/AssessorSignupStepContext.tsx
+++ context/Signup/AssessorSignupStepContext/AssessorSignupStepContext.tsx
@@ -1,33 +1,40 @@
 import { AssessorDetails } from "@components/features/AssessorSignup/AssessorDetails";
 import { AssessorPassword } from "@components/features/AssessorSignup/AssessorPassword";
-import { createContext, ReactNode, useEffect, useState } from "react";
+import { createContext, ReactNode, useEffect, useMemo, useState } from "react";
 
 interface AssessorSignupStepProps {
-    children: ReactNode;
+  children: ReactNode;
+  googleLoginCallback: () => void;
 }
-
-const multistepForm: MultistepForm[] = [
-  {
-    id: 1,
-    name: "Your details",
-    description: "Input your personal details",
-    reactElement: <AssessorDetails />,
-    isSelected: true,
-    disabled: false,
-  },
-  {
-    id: 2,
-    name: "Your password",
-    description: "Create a password for your account",
-    reactElement: <AssessorPassword />,
-    isSelected: false,
-    disabled: true,
-  },
-];
 
 const AssessorSignupStepContext = createContext({} as AssessorSignupStepState);
 
-const AssessorSignupStepProvider = ({ children }: AssessorSignupStepProps) => {
+const AssessorSignupStepProvider = ({
+  children,
+  googleLoginCallback,
+}: AssessorSignupStepProps) => {
+  const multistepForm: MultistepForm[] = useMemo(
+    () => [
+      {
+        id: 1,
+        name: "Your details",
+        description: "Input your personal details",
+        reactElement: <AssessorDetails googleLogin={googleLoginCallback} />,
+        isSelected: true,
+        disabled: false,
+      },
+      {
+        id: 2,
+        name: "Your password",
+        description: "Create a password for your account",
+        reactElement: <AssessorPassword googleLogin={googleLoginCallback} />,
+        isSelected: false,
+        disabled: true,
+      },
+    ],
+    [googleLoginCallback]
+  );
+
   const [forms, setForms] = useState<MultistepForm[]>(multistepForm);
   const [lastEnabledInd, setLastEnabledInd] = useState(1);
   const [selectedId, setSelectedId] = useState(1);
@@ -57,4 +64,4 @@ const AssessorSignupStepProvider = ({ children }: AssessorSignupStepProps) => {
   );
 };
 
-export { AssessorSignupStepProvider, AssessorSignupStepContext }
+export { AssessorSignupStepProvider, AssessorSignupStepContext };
